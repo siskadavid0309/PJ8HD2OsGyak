bb14327707a8ef9f4f73c1694bdf07faa369016a
Siska Dávid (pj8hd2) <90756432+siskadavid0309@users.noreply.github.com>
2022-04-17 23:49:18 +0200

    Add files via upload

---
+++ PJ8HD2_0404/OS_gyak09/OS_gyak09/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace OS_gyak10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string utvonal = @"D:\Visual Studio\repos\OS_gyak10\pj8hd2.txt";
+            if (!File.Exists(utvonal))
+            {
+                using (StreamWriter sw = File.CreateText(utvonal))
+                {
+                    sw.WriteLine("Siska Dávid");
+                    sw.WriteLine("Gazdaságinformatikus");
+                    sw.WriteLine("pj8hd2");
+                }
+            }
+            using (StreamReader sr = File.OpenText(utvonal))
+            {
+                string s;
+                while ((s = sr.ReadLine() )!= null)
+                {
+                    Console.WriteLine(s);
+                }
+               
+            }
+            long length = new FileInfo(utvonal).Length;
+            Console.WriteLine("Beolvasott fájl mérete: "+length+"byte");
+        }
+    }
+}
